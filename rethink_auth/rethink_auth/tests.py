1215e10e3b955d04f8e318343f3e14330ba7c87d
Justyna Ilczuk <att@atte.ro>
2014-10-08 14:24:32 -0700

    Add my wip authorization experiments

---
+++ rethink_auth/rethink_auth/tests.py
@@ -0,0 +1,12 @@
+import datetime
+
+from django.test import TestCase
+
+
+class FirstTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def test_my_things(self):
+        pass
+
--- rethink_auth/users/views.py
+++ rethink_auth/users/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.contrib.auth import authenticate, login
+from django.http.response import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.models import User
+
+
+@csrf_exempt
+def my_view(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(username=username, password=password)
+    first_user = User.objects.first()
+
+    first_user.backend = ''
+    user = first_user
+
+    if user is not None:
+        if user.is_active:
+            login(request, user)
+            # Redirect to a success page.
+        else:
+            # Return a 'disabled account' error message
+            pass
+    else:
+        # Return an 'invalid login' error message.
+        pass
+    return HttpResponse('Secret!', status=200)
